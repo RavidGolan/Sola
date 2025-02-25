87f89726cb3fff6dc30e9d93ef45f105df23760c
Ravid Golan <ravidg@amazon.com>
2025-02-25 17:26:01 +0200

    common

---
+++ Common/Enums/ContentType.ts
@@ -0,0 +1,5 @@
+export enum ContentType {
+    file,
+    dir,
+    symlink
+}
--- Common/Interfaces/IContent.ts
+++ Common/Interfaces/IContent.ts
@@ -3,5 +3,5 @@ import {ContentType} from "../Enums/ContentType";
 export interface IContent {
     name: string,
     type: ContentType,
-    fullPath: string
+    path: string
 }
